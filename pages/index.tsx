305a91c6f61c752d9bebb27d63308a03f2ba9249
Canh Nguyen Tran <canh.nguyen1@ntq-solution.com.vn>
2021-04-17 11:14:05 +0700

    [add] ui button components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,20 @@
 import { IShoppingCart, IUser } from 'src/components/icons';
 import Logo from 'src/assets/svg/logo.svg';
+import { Button } from 'src/components/ui';
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto bg-gray-600">
       <h1 className="font-bold">Hello World</h1>
       <IUser />
       <IShoppingCart />
       <Logo />
+      <Button>Hello World</Button>
+      <Button size="large">Hello World</Button>
+      <Button size="small">Hello World</Button>
+      <Button variant="primary">Hello World</Button>
+      <Button variant="gray">Hello World</Button>
+      <Button variant="link">Hello World</Button>
     </div>
   );
 }
